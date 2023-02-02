cd0d778aa1b9b60a89e76bcfad562651720c5c0f
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-02-02 12:04:11 +0200

    js

---
+++ tren.js
@@ -0,0 +1,7 @@
+function makeMessage (name, price) {
+        const message = `You picked ${name} , price per item is ${price} credits`;
+    
+    // const makeMessage = `You picked ${Scanner}, price per item is 3500 credits`;
+    
+    return ( message );
+  };
